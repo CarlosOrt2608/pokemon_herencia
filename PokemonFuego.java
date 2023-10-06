8fe0f8f80b6a07f7a951b5a815c81937c7a41352
CarlosOrt2608 <132014917+CarlosOrt2608@users.noreply.github.com>
2023-10-06 09:08:34 -0600

    Update PokemonFuego.java

--- PokemonFuego.java
+++ PokemonFuego.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pokemon_herencia;
 
-/**
- *
- * @author Alumno
- */
 public class PokemonFuego extends Pokemon{
     public PokemonFuego(String nombre, int nivel) {
         super(nombre, "Tipo Fuego", nivel);
